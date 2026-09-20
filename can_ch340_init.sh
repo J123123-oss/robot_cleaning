82f53df86af8db44d3fc2cec6461e26ed0edf42a
J123123-oss <J123123-oss@users.noreply.github.com>
2026-09-20 17:27:43 +0800

    chore(repo): update sh

--- can_ch340_init.sh
+++ can_ch340_init.sh
@@ -2,7 +2,9 @@
 # start can0
 # ip link set can0 down
 nmcli r wifi on
-ip link set can0 type can bitrate 1000000 sample-point 0.8 dbitrate 2000000 sample-point 0.8 fd on
+ip link set can0 type can bitrate 500000 sample-point 0.8 dbitrate 2000000 sample-point 0.8 fd on
+# sudo ip link set can0 type can bitrate 500000 restart-ms 0 loopback off
+
 ip link set can0 up
 # 设置can0发送队列长度
 sh -c 'echo 4096 > /sys/class/net/can0/tx_queue_len'
--- motor_start.sh
+++ motor_start.sh
@@ -3,9 +3,9 @@ set -o pipefail
 export PYTHONDONTWRITEBYTECODE=1
 # export ROS_LOCALHOST_ONLY=1
 # GPIO_3控制开启RTK
-gpioset gpiochip3 3=0
+# gpioset gpiochip3 3=0
 
-gpioset gpiochip3 2=0
+# gpioset gpiochip3 2=0
 # 指定需要ping通的目标地址，可根据需要修改
 TARGET_IP="121.40.57.48"
 
